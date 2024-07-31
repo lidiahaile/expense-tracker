73a17f268cb108fd9d37c928750d925ececda2fd
Lidia Haile <lidiahaile@gmail.com>
2024-07-31 18:11:16 +0100

    created user class and annotate as entity

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,11 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
