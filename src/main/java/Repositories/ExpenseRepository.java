eef0e65e1e3f6d83ff822b1db31b8bdf6cdaebc1
Lidia Haile <lidiahaile@gmail.com>
2024-07-31 21:06:38 +0100

    UserRepository and ExpenseRepository are added

--- src/main/java/Repositories/ExpenseRepository.java
+++ src/main/java/Repositories/ExpenseRepository.java
@@ -1,8 +1,9 @@
 package Repositories;
 
-import org.apache.catalina.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExpenseRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List<Expense> findByUserId(Long userId);
 }
--- src/main/java/Repositories/UserRepository.java
+++ src/main/java/Repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package Repositories;
 
-public interface UserRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
