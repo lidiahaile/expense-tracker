1ee36cd0497980c6990c3c870b5d79ffb173e4c6
Lidia Haile <lidiahaile@gmail.com>
2024-07-31 18:14:01 +0100

    Generate getters and setters for User fields

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -8,6 +8,32 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     private String username;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     private String password;
 }
